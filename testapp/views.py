7e1bc427e28b187a143772ea978c7b540dfcd717
sklearn21 <sklearn21@gmail.com>
2021-03-12 09:48:41 +0530

    Completed

--- testapp/views.py
+++ testapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from testapp import forms
+
 
 # Create your views here.
+
+def student_view(request):
+    form = forms.StudentForm()
+    if request.method == 'POST':
+        form = forms.StudentForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            print('Form data saved ino database successfully.')
+
+    return render(request, 'testapp/register.html', {'form': form})
